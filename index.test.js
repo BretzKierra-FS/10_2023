a207da9503af61ba1a414630e731a554e4f28d16
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-10-14 12:57:00 -0400

    Creating Tests
    Creating tests for API

--- index.test.js
+++ index.test.js
@@ -1,3 +1,18 @@
-test('two plus two is four', () => {
-  expect(2 + 2).toBe(4);
+const http = require('axios');
+
+const email = 'kierraBretz@gmail.com';
+const phone = '321-555-3333';
+const birthday = '12/06/2020';
+const password = 'Password!12';
+
+// Testing if API is working
+test('API status is connected...', async () => {
+  try {
+    const response = await http.get('http://localhost:3000/');
+    expect(response.status).toBe(200);
+  } catch (error) {
+    expect(error.code).not.toBe('ECONNREFUSED');
+  }
 });
+
+//
