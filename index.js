faba25abedb9a8d9b2888d27dea9439df2154696
BretzKierra-FS <122029593+BretzKierra-FS@users.noreply.github.com>
2023-10-18 14:58:30 -0400

    Swagger for documentation

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const router = require('./routes/users');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 app.use(
   express.urlencoded({
@@ -11,5 +13,7 @@ app.use(
 app.use(express.json());
 
 app.use('/', router);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(3000, () => console.log('Server is Running'));
